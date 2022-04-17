f2e761532ad6c49265d049b5f5b387e6a9e77281
jsnjjqp <50790914+jsnjjqp@users.noreply.github.com>
2022-04-17 20:54:10 +0800

    Update luqi.js

--- JS/luqi.js
+++ JS/luqi.js
@@ -23,5 +23,5 @@ hostname = www.luqijianggushi.com
 
 let obj = JSON.parse($response.body);
 obj.data.is_vip = 1;
-obj.data.vip_endtime = 1650296877;
+obj.data.vip_endtime = 1690296877;
 $done({body: JSON.stringify(obj)});
